0e8606848f6bd006cf1fd48b893e387b6dfb4060
Joseph <JosephSped23@gmail.com>
2023-11-30 11:03:44 -0500

    completed sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+function submitData (name, email){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+         
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+
+    .then(res => res.json())
+    .then(user => {
+       const h1 = document.createElement('h1')
+       h1.textContent = user.id
+       document.body.appendChild(h1)
+
+        console.log(user.id)
+    })
+    .catch(error =>{
+        alert("ERROR!");
+        const h2 = document.createElement('h2')
+        h2.textContent = error.message
+        document.body.appendChild(h2)
+        
+    })
+}
+// submitData('bob','bob@bob.com')
